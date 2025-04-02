4589cf42fd2c798759b878a8812730f62362cc62
Elio Bonutto <ebonutto@bess-f4r5s6.clusters.42paris.fr>
2025-04-02 16:31:44 +0200

    [Tuple] fix sur normalize

--- libs/libft/src/tuple/operations/normalize_tuple.c
+++ libs/libft/src/tuple/operations/normalize_tuple.c
@@ -6,7 +6,7 @@
 /*   By: ebonutto <ebonutto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:52:45 by ebonutto          #+#    #+#             */
-/*   Updated: 2025/04/02 15:54:42 by ebonutto         ###   ########.fr       */
+/*   Updated: 2025/04/02 16:31:19 by ebonutto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ t_tuple	normalize_tuple(t_tuple a)
 	magnitude = mag_tuple(a);
 	if (magnitude == 0)
 		return new_tuple(0, 0, 0, a.w);
-		return new_tuple(a.x / magnitude, a.y / magnitude, a.z / magnitude, a.w / magnitude);
+	return new_tuple(a.x / magnitude, a.y / magnitude, a.z / magnitude, a.w / magnitude);
 }
