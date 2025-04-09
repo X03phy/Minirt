fc3492c38e358643e5bb49467d4b92b6b0ee206c
Elio Bonutto <ebonutto@bess-f4r3s9.clusters.42paris.fr>
2025-04-09 11:44:00 +0200

    [LUMIERE] fix de certains trucs

--- src/minirt/ray/lighting.c
+++ src/minirt/ray/lighting.c
@@ -6,14 +6,14 @@
 /*   By: ebonutto <ebonutto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 18:30:34 by maecarva          #+#    #+#             */
-/*   Updated: 2025/04/06 12:32:48 by maecarva         ###   ########.fr       */
+/*   Updated: 2025/04/09 11:26:38 by ebonutto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minirt.h"
 
 // vieu
-t_color	lighting2(t_material m, t_light l, t_tuple p, t_tuple eyev, t_tuple normalv, t_config *c)
+t_color lighting2(t_material m, t_light l, t_tuple p, t_tuple eyev, t_tuple normalv, t_config *c, bool in_shadow)
 {
 	t_tuple	effective_color = tuple_multiply(m.color, l.brightness);
 	
@@ -37,8 +37,11 @@ t_color	lighting2(t_material m, t_light l, t_tuple p, t_tuple eyev, t_tuple norm
 			specular = tuple_multiply(white, m.specular * factor);
 		}
 	}
+	if (in_shadow)
+		return (ambient);
 	return (tuple_add(tuple_add(ambient, diffuse), specular));
 	(void)c;
+	(void)in_shadow;
 }
 
 t_color lighting(t_material m, t_light l, t_tuple p, t_tuple eyev, t_tuple normalv, t_config *c, bool in_shadow)
