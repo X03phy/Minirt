8797ae580a5ea88da5119d5ffd654355949092ca
Elio Bonutto <ebonutto@bess-f3r5s12.clusters.42paris.fr>
2025-04-15 12:05:14 +0200

    [CHECKERBOARD] debut

--- minirt_bonus/src/minirt/render/render_obj.c
+++ minirt_bonus/src/minirt/render/render_obj.c
@@ -6,7 +6,7 @@
 /*   By: ebonutto <ebonutto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 09:23:47 by maecarva          #+#    #+#             */
-/*   Updated: 2025/04/14 14:32:34 by ebonutto         ###   ########.fr       */
+/*   Updated: 2025/04/15 11:39:50 by ebonutto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int	render_sphere(t_config *c, t_intersection *xs, t_render *render)
 {
 	t_tuple	color;
+
+	float intensity = define_intensity(xs->object->pattern);
+
+
 
 	ft_bzero(&c->l, sizeof(t_lighting));
 	c->l.normal_vec = vector_normalize(tuple_substitute(render->x_point,
@@ -25,12 +29,18 @@ int	render_sphere(t_config *c, t_intersection *xs, t_render *render)
 	c->l.l = *(c->light);
 	c->l.in_shadow = render->in_shadow;
 	color = lighting(&c->l, c, render->in_shadow);
+	color = tuple_multiply(color, intensity);
 	return (color_to_int(color));
 }
 
 int	render_plane(t_config *c, t_intersection *xs, t_render *render)
 {
 	t_tuple	color;
+	float	intensity;
+
+	intensity = define_intensity(xs->object->pattern);
+	// color = tuple_multiply(color, intensity);
+
 
 	ft_bzero(&c->l, sizeof(t_lighting));
 	c->l.normal_vec = ((t_plane *)xs->object->obj)->orientation_vec;
@@ -40,6 +50,7 @@ int	render_plane(t_config *c, t_intersection *xs, t_render *render)
 	c->l.l = *(c->light);
 	c->l.in_shadow = render->in_shadow;
 	color = lighting(&c->l, c, render->in_shadow);
+	color = tuple_multiply(color, intensity);
 	return (color_to_int(color));
 }
 
